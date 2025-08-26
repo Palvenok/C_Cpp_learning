b2bc1dede6b8a229888928f7000941c7cec2b0fb
Alex <79199642+Palvenok@users.noreply.github.com>
2025-08-26 09:46:18 +0300

    Add files via upload

---
+++ CPP - WinAPI/src/MainWindow.cpp
@@ -0,0 +1,29 @@
+#include "MainWindow.h"
+
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(m_hwnd, &ps);
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+            
+            const wchar_t* text = L"Привет, мир!";
+            TextOutW(hdc, 50, 50, text, lstrlenW(text));
+
+            EndPaint(m_hwnd, &ps);
+        }
+        return 0;
+
+    default:
+        return DefWindowProcW(m_hwnd, uMsg, wParam, lParam);
+    }
+    return TRUE;
+}
--- CPP - WinAPI/src/main.cpp
+++ CPP - WinAPI/src/main.cpp
@@ -1,8 +1,10 @@
+
 #include <main.h>
+#include "MainWindow.h"
 
 int main() 
 {
-    Logger::initialized();
+    //Logger::initialized();
     HINSTANCE hInstance = GetModuleHandle(nullptr);
     int nCmdShow = SW_SHOWDEFAULT;
     return wWinMain(hInstance, nullptr, nullptr, nCmdShow);
@@ -10,67 +12,24 @@ int main()
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) 
 {   
-    const wchar_t CLASS_NAME[] = L"Main window";
-    WNDCLASSW wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    MainWindow win;
 
-    RegisterClassW(&wc);
-
-    HWND hwnd = CreateWindowExW(
-        0,
-        CLASS_NAME,
-        L"Title",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 600, 400,
-        nullptr, nullptr, hInstance, nullptr
-    );
-
-    if (hwnd == nullptr) 
+    if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
     {
-        LOG_ERROR("hwnd creating failure!");
+        LOG_ERROR("Create window error!");
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(win.Window(), nCmdShow);
 
-    MSG msg = {};
-    while (GetMessageW(&msg, nullptr, 0, 0)) 
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+        DispatchMessage(&msg);
     }
 
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
-{
-    switch (uMsg) 
-    {
-        
-        case WM_CLOSE:
-            if (MessageBoxW(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
-                DestroyWindow(hwnd);
-            return 0;
-
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-
-        case WM_PAINT:
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-            const wchar_t* text = L"Привет, мир!";
-            TextOutW(hdc, 50, 50, text, lstrlenW(text));
-
-            EndPaint(hwnd, &ps);
-            return 0;
-
-    }
-    return DefWindowProcW(hwnd, uMsg, wParam, lParam);
 }
